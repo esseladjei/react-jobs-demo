80eda7f0e3aa6bf076d5cf202e0eb057b408bc4b
Essel Adjei-Mensah <adjeiessel@gmail.com>
2024-07-28 16:58:04 +0200

    created application components refactor application component structure added clsx class util lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Developers from './components/HomeCard'
+import BrowserJobs from './components/BrowserJobs'
+import ViewAllJobs from './components/ViewAllJobs'
 function App() {
-  return (
-    <div className='text-5xl text-blue-400'>App</div>
-  )
+   return (
+      <>
+         <Navbar />
+         {/* <!-- Hero --> */}
+         <Hero />
+         {/* <!-- Developers and Employers --> */}
+         <Developers />
+         {/*  <!-- Browse Jobs --> */}
+         <BrowserJobs />
+         {/*  <!-- View all jobs --> */}
+         <ViewAllJobs />
+      </>
+   )
 }
 
 export default App
